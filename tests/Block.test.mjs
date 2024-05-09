b10323c9abbebf8f07e4266d4801a52a05959737
Sanjin Dumisic <codebatine@proton.me>
2024-05-09 16:10:57 +0200

    added some in jest tests :man_juggling:

--- tests/Block.test.mjs
+++ tests/Block.test.mjs
@@ -4,14 +4,14 @@ import Block from '../models/Block.mjs';
 
 const __appdir = dirname(fileURLToPath(import.meta.url));
 
-test('Test for creating a block.', () => {
-  const timestamp = new Date().toISOString();
-  const blockIndex = 0;
-  const previousBlockHash = 'previousHash';
-  const currentBlockHash = 'currentHash';
-  const data = 'data';
-  const difficulty = 5;
-
+function createAndTestBlock(
+  timestamp,
+  blockIndex,
+  previousBlockHash,
+  currentBlockHash,
+  data,
+  difficulty,
+) {
   const block = new Block(
     timestamp,
     blockIndex,
@@ -21,12 +21,46 @@ test('Test for creating a block.', () => {
     difficulty,
   );
 
-  expect(block).toEqual({
+  expect(block.timestamp).toEqual(timestamp);
+  expect(block.blockIndex).toEqual(blockIndex);
+  expect(block.previousBlockHash).toEqual(previousBlockHash);
+  expect(block.currentBlockHash).toEqual(currentBlockHash);
+  expect(block.data).toEqual(data);
+  expect(block.difficulty).toEqual(difficulty);
+
+  return block;
+}
+
+test('Test block creation with default difficulty.', () => {
+  const timestamp = new Date().toISOString();
+  createAndTestBlock(
     timestamp,
-    blockIndex,
-    previousBlockHash,
-    currentBlockHash,
-    data,
-    difficulty,
-  });
+    0,
+    'previousHash',
+    'currentHash',
+    'data',
+    process.env.DIFFICULTY,
+  );
+});
+
+test('Test linking of two blocks.', () => {
+  const timestamp = new Date().toISOString();
+  const block1 = createAndTestBlock(
+    timestamp,
+    0,
+    'previousHash',
+    'currentHash1',
+    'data1',
+    5,
+  );
+  const block2 = createAndTestBlock(
+    timestamp,
+    1,
+    block1.currentBlockHash,
+    'currentHash2',
+    'data2',
+    5,
+  );
+
+  expect(block2.previousBlockHash).toEqual(block1.currentBlockHash);
 });
